df9bfa2494513909e659e2b2a148fe2621259228
pzaydulin <pzaydulin@gmail.com>
2024-10-09 22:34:50 -0700

    #2 installed PrimeNG, Tailwind, created admin layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, RippleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'platzi-store';
+export class AppComponent implements OnInit {
+  private primengConfig: PrimeNGConfig = inject(PrimeNGConfig);
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }  
 }
