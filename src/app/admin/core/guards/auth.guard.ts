cdbd408087b71f57f0784d0784e680e2860cb93c
pzaydulin <pzaydulin@gmail.com>
2024-11-10 03:49:40 -0800

    #14 unsubscribe in guards

--- src/app/admin/core/guards/auth.guard.ts
+++ src/app/admin/core/guards/auth.guard.ts
@@ -1,16 +1,24 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { TokenService } from '../services/token.service';
 import { inject } from '@angular/core';
+import { Subject } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
 
+  const destroy$ = new Subject();
+
   inject(TokenService).isAuthenticated.subscribe({
     next: (value) => {
-      console.log(value);
-      
       if (!value) {
         inject(Router).navigate(['']);
       }
+    },
+    error: (err) => {
+        inject(Router).navigate(['']);
+    },
+    complete: () => {
+      destroy$.next(true);
+      destroy$.unsubscribe();
     }
   });
 
--- src/app/admin/core/guards/guest.guard.ts
+++ src/app/admin/core/guards/guest.guard.ts
@@ -1,13 +1,24 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { TokenService } from '../services/token.service';
+import { Subject } from 'rxjs';
 
 export const guestGuard: CanActivateFn = (route, state) => {
+
+  const destroy$ = new Subject();
+
   inject(TokenService).isAuthenticated.subscribe({
     next: (value) => {
       if (value) {
         inject(Router).navigate(['products']);
       }
+    },
+    error: (err) => {
+      inject(Router).navigate(['']);
+    },
+    complete: () => {
+      destroy$.next(true);
+      destroy$.unsubscribe();
     },
   });
 
