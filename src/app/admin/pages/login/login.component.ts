1d45abfdd1767c7cfd9c6f1017c050c3f93f0f78
pzaydulin <pzaydulin@gmail.com>
2024-11-15 22:40:06 -0800

    #17 PrimeNG in Login, install NgRx

--- src/app/admin/pages/login/login.component.ts
+++ src/app/admin/pages/login/login.component.ts
@@ -1,30 +1,50 @@
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup,  ReactiveFormsModule,  Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from '../../core/services/auth.service';
 import { Subject, takeUntil } from 'rxjs';
 import { Router } from '@angular/router';
+import { PanelModule } from 'primeng/panel';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { PasswordModule } from 'primeng/password';
+import { MessageService } from 'primeng/api';
+import { Toast, ToastModule } from 'primeng/toast';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    PanelModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
+    PasswordModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  providers: [MessageService],
 })
 export class LoginComponent implements OnInit, OnDestroy {
-
   private authService: AuthService = inject(AuthService);
   private router: Router = inject(Router);
+  private messageService = inject(MessageService);
 
   protected destroy$ = new Subject();
   protected loginForm!: FormGroup;
 
-  constructor(private fb:FormBuilder) {}
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       username: ['', [Validators.required]],
-      password: ['', [Validators.required, Validators.minLength(8)]]
+      password: ['', [Validators.required, Validators.minLength(8)]],
     });
   }
 
@@ -35,14 +55,22 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   onLogin() {
     if (this.loginForm.valid) {
-      this.authService.login(this.loginForm.value)
+      this.authService
+        .login(this.loginForm.value)
         .pipe(takeUntil(this.destroy$))
         .subscribe({
           next: (res) => {
-            if(res) {
+            if (res) {
               this.router.navigate(['products']);
             }
-          }
+          },
+          error: (err: HttpErrorResponse) => {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: `User Not Found (${err.statusText})`,
+            });
+          },
         });
     } else {
       this.loginForm.markAllAsTouched();
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,14 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimations(),
-    provideHttpClient()
-  ]
+    provideHttpClient(),
+    provideStore()
+]
 };
